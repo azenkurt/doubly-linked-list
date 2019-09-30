9026347bbcfc05365f6f62d2a5a8b733c69d3034
azenkurt <azenkurit@gmail.com>
2019-09-30 14:32:40 +0300

    init: sol 0

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,83 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+    }
 
-    append(data) {}
+    append( data ) {
+        let nod = new Node ( data );
+        if ( !this._head ) {
+          this._head = nod;
+          this._tail = nod;             
+        }
+        else {
+          nod.prev = this._tail;
+          this._tail.next = nod;
+          this._tail = nod;
+        }       
+        this.length += 1;             
+    }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        let counter = 0;
+        let cur = this._head;
+        while ( counter < index ) {            
+            cur = cur.next;
+            counter += 1;
+        }
+        return cur.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+    
+    }
+    
+    isEmpty() {
+        return this.length === 0 ? true : false;
+    }
 
-    isEmpty() {}
+    clear() {
+        let cur = this._head;
+        for ( let i=0; i<this.length; i++ ) {
+            this._head = null;
+            this._head = cur.next;            
+        }
+    }
 
-    clear() {}
-
-    deleteAt(index) {}
+    deleteAt(index) {
+        let cur = this._head;
+        for ( let i=0; i<index; i++ ) {
+            cur = cur.next;            
+        }
+        for ( let j=this.length - index; j<this.length; j++ ) {
+            
+        }
+    }
 
     reverse() {}
 
-    indexOf(data) {}
+    indexOf(data) {
+        let counter = 0;
+        let cur = this._head;
+        let rez = -1;
+        while ( counter < this.length ) {            
+            if ( cur.data === data ) { rez = counter; }
+            cur = cur.next;
+            counter += 1;
+        }
+        return rez;
+    }
 }
 
 module.exports = LinkedList;
